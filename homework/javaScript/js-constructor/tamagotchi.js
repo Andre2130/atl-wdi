f7e2ea0fa260bd32fed5b969ff8f6a52846dabcd
Andre Robinson <andrerobinson@Andres-MacBook-Pro.local>
2017-09-24 19:50:34 -0400

    having trouble with player.js

--- homework/javaScript/js-constructor/tamagotchi.js
+++ homework/javaScript/js-constructor/tamagotchi.js
@@ -23,6 +23,25 @@ class Tamagotchies {
         console.log("blahhh!");
         console.log(this.name + ' has current restedness = ' + this.restedness)
       }
+      start(){
+        console.log("Starting " + this.name);
+        var self = this;
+        this.hungerTimer = setInterval(function() {
+          self.cry();
+        }, 6000);
+        this.yawnTimer = setInterval(function() {
+          self.yawn();
+        }, 10000);
+        this.sickTimer = setInterval(function() {
+          self.puke();
+        }, 25000);
+    }
+    stop(){
+        console.log("Stopping " + this.name);
+        clearInterval(this.hungerTimer);
+        clearInterval(this.yawnTimer);
+        clearInterval(this.sickTimer);
+      }
   }
 
 
@@ -33,3 +52,5 @@ var constructedObject2 = new Tamagotchies();
 //test out your Tamagotchies below via console.logs
 console.log(constructedObject1);
 console.log(constructedObject2);
+var bob = new Tamagotchies('bob', 'blah')
+bob.start();
