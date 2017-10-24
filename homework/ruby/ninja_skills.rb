e5a107d4ad4fc2f8abdb385b413e460399a26e39
Andre Robinson <andrerobinson@Andres-MacBook-Pro.local>
2017-10-24 04:20:33 -0400

    4. String skills

--- homework/ruby/ninja_skills.rb
+++ homework/ruby/ninja_skills.rb
@@ -3,18 +3,23 @@ b = false
 puts a = b
 puts a == b
 
+
 p = nil
 puts "#{p} was here"
 
+
 d = 4
 puts d
-
 e = 2.3
 puts e
-
 d == e
 puts d
+
 
 christine = "Hi I'm Christine"
 likes_to = "long walks on the beach, kinda."
 puts "#{christine} and I like #{likes_to}"
+g = "2"
+gToNumber = 2
+g + gToNumber.to_s
+
